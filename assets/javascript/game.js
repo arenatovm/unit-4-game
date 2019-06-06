48669baa63e2fce9d895d5f437f183be1849da72
Andres Vera <arenatovm@gmail.com>
2019-06-05 23:55:51 -0400

    game.js initialized

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+    var wins = 0;
+    var loses = 0;
+    var score = 0;
+
+    function goalnumber() {
+        var goalnum =  Math.floor((Math.random() * 120) + 19);
+        goalnum = document.getElementById("goal").innerHTML;
+
+    }
+    function crystalnumber (){
+        var crystalnum = Math.floor((Math.random() * 12) + 1);
+        crystalnum = document.getElementById()
+    }
+    
